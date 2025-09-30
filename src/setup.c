c349a5fcddb0b21e9d32565221a23665c0176c34
pwam <ryandclements@gmail.com>
2025-09-30 14:45:40 -0700

    Added basic setup files; defined preliminary Init and Shutdown functions.

---
+++ src/setup.c
@@ -0,0 +1,17 @@
+#include "raylib.h"
+#include "setup.h"
+#include "ui.h"
+
+void InitGame(void){
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
+    InitAudioDevice();
+
+    SetExitKey(KEY_NULL);
+    SetTargetFPS(60);
+
+}
+
+void ShutdownGame(void){
+    CloseAudioDevice();
+    CloseWindow();
+}
