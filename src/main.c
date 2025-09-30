febcae249e2e894e82ed353eac0296aba71f6ddb
pwam <ryandclements@gmail.com>
2025-09-30 14:46:54 -0700

    Added preliminary logic for setting up game and opening/closing window. All largely placeholder.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include "raylib.h"
+#include "setup.h"
 
 int main(void)
 {
+	InitGame();
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+			ClearBackground(RAYWHITE);
+
+			DrawText("hello, world", 190, 200, 20, LIGHTGRAY);
+
+		EndDrawing();
+	}
+
+	ShutdownGame();
+	
 	return 0;
 }
