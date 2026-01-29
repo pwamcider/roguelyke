85ed11fc3f192cb183039a40ff09f93b8dd6eae4
pwam <ryandclements@gmail.com>
2026-01-29 08:13:00 -0800

    Moved example monster structs into new monster files for organization.

---
+++ include/monsters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// ------------------------------------------------------------
+
+typedef struct Slime {
+	int health;
+} Slime;
+
+typedef struct Goblin {
+	int health;
+} Goblin;
---
+++ src/monsters.c
@@ -0,0 +1,15 @@
+#include "monsters.h"
+#include "world.h"
+
+// ------------------------------------------------------------
+
+// Sample definition of goblin example using .data sytax.
+/*
+Entity MyGoblin = {
+    .icon = "g",
+    .data.goblin = {
+        .health = 50,
+    },
+    .type = MONSTER,
+};
+*/
