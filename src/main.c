553f16b59ba33d72b89a6a2ac9cfec9c1abcb053
pwam <ryandclements@gmail.com>
2026-01-30 12:36:40 -0800

    Added #includes needed for new functions; added DrawField() call.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
+#include "draw.h"
 #include "raylib.h"
 #include "resources.h"
 #include "setup.h"
+#include "update.h"
 #include "world.h"
 
 // ------------------------------------------------------------
@@ -11,12 +13,15 @@ int main(void)
 
 	LoadResources();
 
+	ClearField();
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 
 			ClearBackground(BLACK);
 
+			/*
 			DrawText("hello, world", 190, 200, 20, WHITE);
 
 			for (int i = 0; i < 40; i++)
@@ -25,7 +30,11 @@ int main(void)
 			}
 
 			DrawText("@", 500, 500, 20, WHITE);
-			// DrawText(GameField.cells[0][0].entity->icon, 600, 600, 20, WHITE);
+			DrawText(GameField.cells[0][0].entity->icon, 600, 600, 20, WHITE);
+			*/
+
+			DrawField();
+
 			
 		EndDrawing();
 	}
