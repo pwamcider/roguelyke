b431bc2997048ff02baf6642f6d999b2ab9d43d6
pwam <ryandclements@gmail.com>
2026-01-30 12:37:40 -0800

    Wrote preliminary ClearField() code to set all cells to GROUND type.

--- src/update.c
+++ src/update.c
@@ -1,4 +1,14 @@
 #include "update.h"
+#include "world.h"
 
 // ------------------------------------------------------------
 
+void ClearField(void) {
+    for (int x = 0; x < fieldSizeX; x++)
+    {
+        for (int y = 0; y < fieldSizeY; y++)
+        {
+            World.cells[x][y].cellType = GROUND;
+        }
+    }
+};
