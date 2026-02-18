adf5f98f7c60771a43005e51d1e47f601df69a12
pwam <ryandclements@gmail.com>
2026-02-18 09:27:10 -0800

    No impact; removed old placeholder comment.

--- src/main.c
+++ src/main.c
@@ -29,18 +29,6 @@ int main(void)
 
 			ClearBackground(BLACK);
 
-			/*
-			DrawText("hello, world", 190, 200, 20, WHITE);
-
-			for (int i = 0; i < 40; i++)
-			{
-				DrawText(".", (200 + (i*20)), 300, 20, WHITE);
-			}
-
-			DrawText("@", 500, 500, 20, WHITE);
-			DrawText(GameField.cells[0][0].entity->icon, 600, 600, 20, WHITE);
-			*/
-
 			UpdateGameState();
 			
 			DrawWorld();
