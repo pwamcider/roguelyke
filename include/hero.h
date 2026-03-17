b291ff431bc29c6daea0e557131bbb2000de5e7f
pwam <ryandclements@gmail.com>
2026-03-17 09:27:25 -0700

    Created new hero files; set up GenerateHero().

---
+++ include/hero.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "world.h"
+
+// Function Prototypes
+// ------------------------------------------------------------
+
+Entity* GenerateHero(void);
---
+++ src/hero.c
@@ -0,0 +1,20 @@
+#include "hero.h"
+#include "stdlib.h"
+
+// Hero Generation
+// ------------------------------------------------------------
+
+Entity* GenerateHero(void) {
+    Entity* hero = malloc(sizeof(Entity));
+
+    hero->type = HERO;
+    // Visuals
+    hero->icon = "@";
+    hero->color = WHITE;
+    // Starting Attributes
+    hero->data.hero.health = 10;
+    hero->data.hero.strength = 5;
+    hero->data.hero.dex = 4;
+
+    return hero;
+};
