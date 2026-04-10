a9ca513497c9f51dfdb5af4a518692b22ec0eff9
pwam <ryandclements@gmail.com>
2026-04-10 08:32:47 -0700

    GD: adjusted Oak Tree dodge value.

--- src/creatures.c
+++ src/creatures.c
@@ -5,16 +5,22 @@
 // ------------------------------------------------------------
 
 // TODO - make the tree less dodgy.
+// Consider making tree an inanimate object once monsters are in.
+
 Entity* GenerateOakTree(void) {
     Entity* oakTree = malloc(sizeof(Entity));
-
     oakTree->type = CREATURE;
+
+    // Player-facing Name
+    oakTree->logName = "oak tree";
+
     // Visuals
     oakTree->icon = "T";
     oakTree->color = GREEN;
+
     // Starting Attributes
     oakTree->data.creature.health = 30;
-    oakTree->data.creature.dodge = 15;
+    oakTree->data.creature.dodge = 5;
 
     return oakTree;
 };
