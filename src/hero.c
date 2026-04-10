b8fb407feabbc0db5bc06c92644edb06b3857bee
pwam <ryandclements@gmail.com>
2026-04-10 08:32:29 -0700

    GD: adjusted Hero starting dex.

--- src/hero.c
+++ src/hero.c
@@ -8,13 +8,15 @@ Entity* GenerateHero(void) {
     Entity* hero = malloc(sizeof(Entity));
 
     hero->type = HERO;
+
     // Visuals
     hero->icon = "@";
     hero->color = WHITE;
+    
     // Starting Attributes
     hero->data.hero.health = 10;
     hero->data.hero.strength = 5;
-    hero->data.hero.dex = 4;
+    hero->data.hero.dex = 3;
 
     return hero;
 };
